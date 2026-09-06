a2991807218f6983f10253d533c881c1f58eb36a
quangtm198-pt <EMAIL_GITHUB_MI>
2026-09-06 21:53:08 +0700

    update full project

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
   description: "Hệ thống Phòng họp không giấy",
 
   icons: {
-    icon: "/icon.png",
-    apple: "/apple-icon.png",
+    icon: "/icon.png?v=2",
+    apple: "/apple-icon.png?v=2",
   },
 };
 
