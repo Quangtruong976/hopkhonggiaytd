09d1cd943ca7ae8a3dc85932c9dd084932a41b6d
quangtm198-pt <EMAIL_GITHUB_MI>
2026-09-13 18:58:08 +0700

    update full project

--- app/dang-nhap/page.tsx
+++ app/dang-nhap/page.tsx
@@ -260,7 +260,7 @@ export default function DangNhapPage() {
   <img
     src="/logo-doan.png"
     alt="Logo Đoàn TNCS Hồ Chí Minh"
-    className="h-25 w-25 object-contain"
+    className="h-30 w-30 object-contain"
   />
 </div>
 
